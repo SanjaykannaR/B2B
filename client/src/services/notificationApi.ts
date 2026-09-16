29edfdf329542e699866fc1ec28475b8b2cf3ceb
Sanjay_kanna <165680113+SanjaykannaR@users.noreply.github.com>
2026-09-16 16:26:56 +0530

    feat: add admin and client navigation components and invoice generator service

--- client/src/services/notificationApi.ts
+++ client/src/services/notificationApi.ts
@@ -4,6 +4,14 @@
 
 import api from './api';
 
+export interface NotificationItem {
+  _id: string;
+  title: string;
+  message: string;
+  isRead: boolean;
+  createdAt: string;
+}
+
 /**
  * Retrieves a list of in-app notifications for the logged-in user.
  * @returns Promise with list of notifications
